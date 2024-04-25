61a2d04b4d9b5b17d7fcca914a7a20665eb3aa7b
habtard <habtard@gmail.com>
2024-04-25 11:16:57 +0100

    Few updates

--- migrations/01_player_navigator_settings.sql
+++ migrations/01_player_navigator_settings.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `player_navigator_settings` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `player_id` bigint(20) unsigned NOT NULL,
-  `window_x` int(11) NOT NULL,
-  `window_y` int(11) NOT NULL,
-  `window_width` int(11) NOT NULL,
-  `window_height` int(11) NOT NULL,
-  `open_searches` tinyint(1) NOT NULL,
+  `window_x` int(11) NOT NULL DEFAULT 50,
+  `window_y` int(11) NOT NULL DEFAULT 50,
+  `window_width` int(11) NOT NULL DEFAULT 435,
+  `window_height` int(11) NOT NULL DEFAULT 535,
+  `open_searches` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `player_navigator_settings_player_id_foreign` (`player_id`),
   CONSTRAINT `player_navigator_settings_player_id_foreign` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)

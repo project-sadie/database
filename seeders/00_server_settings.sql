991d3457441aa60cba1fcc38a6897f710e1b60db
habtard <habtard@gmail.com>
2024-05-14 11:10:10 +0100

    More updates

--- seeders/00_server_settings.sql
+++ seeders/00_server_settings.sql
@@ -1,2 +1,2 @@
-INSERT INTO `server_settings` (`player_welcome_message`, `make_currency_rewards_fair`) VALUES 
-('Welcome (back) to Sadie [username], we\'re running version [version]!',1);
+INSERT INTO `server_settings` (`player_welcome_message`, `make_currency_rewards_fair`, `created_at`) VALUES 
+('Welcome (back) to Sadie [username], we\'re running version [version]!', 1, NOW());

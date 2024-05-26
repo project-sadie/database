f2ef529da0a10e5c428d5b235e28346e8f7d643b
habtard <habtard@gmail.com>
2024-05-26 10:31:57 +0100

    Fixed naming

--- migrations/01_groups.sql
+++ migrations/01_groups.sql
@@ -8,5 +8,5 @@ CREATE TABLE `groups` (
   PRIMARY KEY (`id`),
   KEY `groups_player_id_foreign` (`player_id`),
   CONSTRAINT `groups_player_id_foreign` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`),
-  CONSTRAINT `player_room_likes_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
+  CONSTRAINT `groups_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

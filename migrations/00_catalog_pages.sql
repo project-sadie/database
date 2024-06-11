80d6355f3f243a8f647f1a277098a4987adbc05c
habtard <habtard@gmail.com>
2024-06-11 14:06:21 +0100

    Updates

--- migrations/00_catalog_pages.sql
+++ migrations/00_catalog_pages.sql
@@ -4,12 +4,15 @@ CREATE TABLE `catalog_pages` (
   `caption` varchar(255) NOT NULL,
   `layout` varchar(255) NOT NULL,
   `role_id` int(11) DEFAULT NULL,
-  `catalog_page_id` bigint(20) NOT NULL,
+  `catalog_page_id` bigint(20) NULL,
   `order_id` int(11) NOT NULL,
   `icon_id` int(11) NOT NULL,
   `enabled` tinyint(1) NOT NULL,
   `visible` tinyint(1) NOT NULL,
   `images_json` JSON NOT NULL,
   `texts_json` JSON NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `catalog_pages_catalog_page_id_foreign` (`catalog_page_id`),
+  CONSTRAINT `catalog_pages_catalog_page_id_foreign` FOREIGN KEY (`catalog_page_id`) REFERENCES `catalog_pages` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+

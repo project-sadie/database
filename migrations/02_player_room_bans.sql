15de4fa7dafc19414219fe6ee250c6d551d1bb61
habtard <habtard@gmail.com>
2024-06-13 22:55:04 +0100

    Updates

---
+++ migrations/02_player_room_bans.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `player_room_bans` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `player_id` bigint(20) unsigned NOT NULL,
+  `room_id` bigint(20) unsigned NOT NULL,
+  `expires_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `player_room_bans_player_id_foreign` (`player_id`),
+  CONSTRAINT `player_sso_tokens_player_id_foreign` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`),
+  KEY `player_room_bans_room_id_foreign` (`room_id`),
+  CONSTRAINT `player_sso_tokens_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

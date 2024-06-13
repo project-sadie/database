dc7c49e8a2fd9d093fc4b2256401341cafc634d1
habtard <habtard@gmail.com>
2024-06-13 22:34:32 +0100

    Updates

--- seeders/00_permissions.sql
+++ seeders/00_permissions.sql
@@ -1,8 +1,10 @@
 INSERT INTO `permissions` (`id`, `name`) VALUES 
-(1,'moderation_tools'),
-(2,'command_shutdown'),
-(3,'command_hotel_alert'),
-(4,'command_user_info'),
-(5,'command_kick'),
-(6,'command_kick_all'),
-(7,'command_unload');
+('moderation_tools'),
+('command_shutdown'),
+('command_hotel_alert'),
+('command_user_info'),
+('command_kick'),
+('command_kick_all'),
+('command_unload'),
+('any_room_owner'),
+('any_room_rights');

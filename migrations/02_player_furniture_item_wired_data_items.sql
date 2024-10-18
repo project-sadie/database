c5f0358e768b02136b4b968d4e5e74c2760969b4
ashdevelops <ash.smith.dev@gmail.com>
2024-10-18 15:44:21 +0100

    Changes

--- migrations/02_player_furniture_item_wired_data_items.sql
+++ migrations/02_player_furniture_item_wired_data_items.sql
@@ -7,12 +7,15 @@ CREATE TABLE `player_furniture_item_wired_data_items` (
         `player_furniture_item_placement_data_id`
     ),
     KEY `pfiwdi_duo_index` (
-        `player_furniture_item_wired_data_id`, `player_furniture_item_placement_data_id`
+        `player_furniture_item_wired_data_id`, 
+        `player_furniture_item_placement_data_id`
     ),
-    CONSTRAINT `pfiwdi_p_furniture_item_placement_data_id_foreign` FOREIGN KEY (
+    CONSTRAINT `pfiwdi_p_furniture_item_placement_data_id_foreign` 
+    FOREIGN KEY (
         `player_furniture_item_placement_data_id`
     ) REFERENCES `player_furniture_item_placement_data` (`id`),
-    CONSTRAINT `pfiwdi_player_furniture_item_wired_data_id_foreign` FOREIGN KEY (
+    CONSTRAINT `pfiwdi_player_furniture_item_wired_data_id_foreign` 
+    FOREIGN KEY (
         `player_furniture_item_wired_data_id`
     ) REFERENCES `player_furniture_item_wired_data` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;

b83f0e04301736c20432e49e9f6a83c1c630e2d4
ashdevelops <ash.smith.dev@gmail.com>
2024-12-13 22:23:43 +0000

    Changes

--- seeders/00_server_settings.sql
+++ seeders/00_server_settings.sql
@@ -1,8 +1,10 @@
 INSERT INTO `server_settings` (
-    `player_welcome_message`, `make_currency_rewards_fair`, `created_at`
+    `player_welcome_message`, 
+    `fair_currency_rewards`, 
+    `created_at`
 ) VALUES
 (
-    "Welcome (back) to Sadie [username], we're running version [version]!",
+    'Welcome (back) to Sadie [username], we''re running version [version]!',
     1,
     NOW()
 );

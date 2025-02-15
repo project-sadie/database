349495dbf10251523f8444ce176499acae6ec403
ashdevelops <ash.smith.dev@gmail.com>
2025-02-15 18:21:59 +0000

    wired updates

---
+++ migrations/03_player_furniture_item_wired_parameters.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `player_furniture_item_wired_parameters` (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `data` int(11) unsigned NOT NULL,
+    `player_furniture_item_wired_data_id` bigint(20) unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `pfiwp_pfiwd_id_foreign` (`player_furniture_item_wired_data_id`),
+    CONSTRAINT `pfiwp_pfiwd_id_foreign` FOREIGN KEY (
+        `player_furniture_item_wired_data_id`
+) REFERENCES `player_furniture_item_wired_data` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;

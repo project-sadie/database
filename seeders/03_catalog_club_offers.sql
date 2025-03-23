03e180631bd7fa150d56c633bb142303a0f163ee
ashdevelops <ash.smith.dev@gmail.com>
2025-03-23 13:30:25 +0000

    fixes

--- seeders/03_catalog_club_offers.sql
+++ seeders/03_catalog_club_offers.sql
@@ -1,9 +1,9 @@
 INSERT INTO `catalog_club_offers` (
-    `name`, 
-    `duration_days`, 
-    `cost_credits`, 
-    `cost_points`, 
-    `cost_points_type`, 
+    `name`,
+    `duration_days`,
+    `cost_credits`,
+    `cost_points`,
+    `cost_points_type`,
     `is_vip`
 ) VALUES ('1 Month HC', 31, 250, 0, 0, 1),
 ('3 Month HC', 93, 600, 0, 0, 1);

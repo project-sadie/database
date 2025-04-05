2fcdf000210b1bc0ec275069a03c69ec98b5b689
ashdevelops <ash.smith.dev@gmail.com>
2025-04-05 19:01:53 +0100

    updates to columns

--- migrations/02_banned_ip_addresses.sql
+++ migrations/02_banned_ip_addresses.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `banned_ip_addresses` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `owner_id` bigint(20) unsigned NULL,
+    `creator_id` bigint(20) unsigned NULL,
     `reason` varchar(255) NOT NULL,
     `ip_address` varchar(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
     `expires_at` TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY `banned_ip_addresses_owner_id_foreign` (`owner_id`),
-    CONSTRAINT `banned_ip_addresses_owner_id_foreign` FOREIGN KEY (
-        `owner_id`
+    KEY `banned_ip_addresses_creator_id_foreign` (`creator_id`),
+    CONSTRAINT `banned_ip_addresses_creator_id_foreign` FOREIGN KEY (
+        `creator_id`
     ) REFERENCES `players` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
--- migrations/02_player_bans.sql
+++ migrations/02_player_bans.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `player_bans` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `owner_id` bigint(20) unsigned NOT NULL,
+    `creator_id` bigint(20) unsigned NOT NULL,
     `player_id` bigint(20) unsigned NOT NULL,
     `reason` varchar(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
     `expires_at` TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY `player_bans_owner_id_foreign` (`owner_id`),
+    KEY `player_bans_creator_id_foreign` (`creator_id`),
     KEY `player_bans_player_id_foreign` (`player_id`),
-    CONSTRAINT `player_bans_owner_id_foreign` FOREIGN KEY (
-        `owner_id`
+    CONSTRAINT `player_bans_creator_id_foreign` FOREIGN KEY (
+        `creator_id`
     ) REFERENCES `players` (`id`),
     CONSTRAINT `player_bans_player_id_foreign` FOREIGN KEY (
         `player_id`
